0b13b7c54f9645ff8deee79b0a5f690f74c7710b
SmileeA <1290539974@qq.com>
2024-03-30 22:42:43 +0000

    asdasd

--- Server.cpp
+++ Server.cpp
@@ -28,7 +28,8 @@ struct TableData
 
 struct MyShared
 {
-    std::vector<TableData> table = std::vector<TableData>(3);
+    int test;
+    // std::vector<TableData> table = std::vector<TableData>(3);
     // std::vector<int> dealerHand1 = std::vector<int>(10);
     // std::vector<int> playerHand1 = std::vector<int>(10);
     // std::vector<int> dealerHand2 = std::vector<int>(10);
@@ -354,185 +355,178 @@ public:
         // Game logic goes here. For now, just a placeholder.
         // Simulate game room activity
         std::cout << "2:::" << std::endl;
-        try {
-    Shared<MyShared> shared("sharedMemory");
-    std::cout << "Accessed shared memory successfully." << std::endl;
-} catch (const std::string& e) {
-    std::cerr << "Exception caught: " << e << std::endl;
-} catch (const std::exception& e) {
-    std::cerr << "Standard exception caught: " << e.what() << std::endl;
-} catch (...) {
-    std::cerr << "Unknown exception caught" << std::endl;
-}
 
-        // try
-        // {
-        //     Shared<MyShared> shared("sharedMemory1");
+        try
+        {
+            Shared<MyShared> shared("sharedMemory");
 
-        //     std::cout << "2" << std::endl;
-        //     // used for a random generate a int num
-        //     // srand(time(NULL));
+            shared->test = 3;
+            std::cout << "2" << std::endl;
+            // used for a random generate a int num
+            // srand(time(NULL));
+            std::cout << shared->test << std::endl;
 
-        //     try
-        //     {
-        //         // initiali case
-        //         continueFlag = true;
-        //         while (continueFlag == true)
-        //         {
-        //             std::cout << "Starting a new game..." << std::endl;
-        //             // initialize two card to the shared memory
-        //             gamePlayer->deal(deck[0][5]);
-        //             std::cout << "1" << std::endl;
+            try
+            {
+                // initiali case
+                continueFlag = true;
+                while (continueFlag == true)
+                {
+                    std::cout << "Starting a new game..." << std::endl;
+                    // initialize two card to the shared memory
+                    gamePlayer->deal(deck[0][5]);
+                    std::cout << "1" << std::endl;
 
-        //             // shared->playerHand1 = this->gamePlayer->getHand();
-        //             // std::cout << "1" << std::endl;
-        //             // shared->playerHand1.push_back(deck[rand() % 4][rand() % 13]);
-        //             // std::cout << "1" << std::endl;
-        //             // shared->dealerHand1.push_back(deck[rand() % 4][rand() % 13]);
-        //             // std::cout << "1" << std::endl;
-        //             // shared->dealerHand1.push_back(deck[rand() % 4][rand() % 13]);
-        //             // std::cout << "1" << std::endl;
+                    // shared->playerHand1 = this->gamePlayer->getHand();
+                    // std::cout << "1" << std::endl;
+                    // shared->playerHand1.push_back(deck[rand() % 4][rand() % 13]);
+                    // std::cout << "1" << std::endl;
+                    // shared->dealerHand1.push_back(deck[rand() % 4][rand() % 13]);
+                    // std::cout << "1" << std::endl;
+                    // shared->dealerHand1.push_back(deck[rand() % 4][rand() % 13]);
+                    // std::cout << "1" << std::endl;
 
-        //             for (auto *spectator : Spectatorlist)
-        //             {
-        //                 spectator->send(shared);
-        //             }
+                    for (auto *spectator : Spectatorlist)
+                    {
+                        spectator->send(shared);
+                    }
 
-        //             gamePlayer->readHand(shared);
+                    gamePlayer->readHand(shared);
 
-        //             // player hit the card
-        //             while (true)
-        //             {
+                    // player hit the card
+                    while (true)
+                    {
 
-        //                 gamePlayer->askHit();
-        //                 if (gamePlayer->getHitFlag())
-        //                 {
-        //                     // shared->playerHand1.push_back(deck[rand() % 4][rand() % 13]);
-        //                 }
-        //                 else if (gamePlayer->getHitFlag() == false)
-        //                 {
-        //                     break;
-        //                 };
+                        gamePlayer->askHit();
+                        if (gamePlayer->getHitFlag())
+                        {
+                            // shared->playerHand1.push_back(deck[rand() % 4][rand() % 13]);
+                        }
+                        else if (gamePlayer->getHitFlag() == false)
+                        {
+                            break;
+                        };
 
-        //                 if (gamePlayer->calculateHandTotal() > 21)
-        //                 {
-        //                     gamePlayer->setExplode(true);
-        //                     break;
-        //                 };
+                        if (gamePlayer->calculateHandTotal() > 21)
+                        {
+                            gamePlayer->setExplode(true);
+                            break;
+                        };
 
-        //                 for (auto *spectator : Spectatorlist)
-        //                 {
-        //                     spectator->send(shared);
-        //                 }
-        //             }
+                        for (auto *spectator : Spectatorlist)
+                        {
+                            spectator->send(shared);
+                        }
+                    }
 
-        //             // dealer begin hit the card
+                    // dealer begin hit the card
 
-        //             while ((!gamePlayer->getExplode() && gameDealer->calculateHandTotal() < 17) || (gameDealer->calculateHandTotal() < 21))
-        //             {
-        //                 // if dealer satisfy the condition then add card to shared memory
-        //                 // shared->dealerHand1.push_back(deck[rand() % 4][rand() % 13]);
-        //                 gameDealer->deal(shared);
-        //                 // let spector get information
-        //                 for (auto *spectator : Spectatorlist)
-        //                 {
-        //                     spectator->send(shared);
-        //                 }
-        //                 // since the player has finished his round and act as a spectator
-        //                 gamePlayer->send(shared);
-        //             }
+                    while ((!gamePlayer->getExplode() && gameDealer->calculateHandTotal() < 17) || (gameDealer->calculateHandTotal() < 21))
+                    {
+                        // if dealer satisfy the condition then add card to shared memory
+                        // shared->dealerHand1.push_back(deck[rand() % 4][rand() % 13]);
+                        gameDealer->deal(shared);
+                        // let spector get information
+                        for (auto *spectator : Spectatorlist)
+                        {
+                            spectator->send(shared);
+                        }
+                        // since the player has finished his round and act as a spectator
+                        gamePlayer->send(shared);
+                    }
 
-        //             // if player exploded
-        //             if (gamePlayer->getExplode())
-        //             {
-        //                 // dealer wins, notify player and all spectators
-        //                 gamePlayer->sendWinner("Dealer wins!!");
+                    // if player exploded
+                    if (gamePlayer->getExplode())
+                    {
+                        // dealer wins, notify player and all spectators
+                        gamePlayer->sendWinner("Dealer wins!!");
 
-        //                 for (auto *spectator : Spectatorlist)
-        //                 {
-        //                     spectator->sendWinner("Dealer wins!!");
-        //                 }
-        //             }
+                        for (auto *spectator : Spectatorlist)
+                        {
+                            spectator->sendWinner("Dealer wins!!");
+                        }
+                    }
 
-        //             // if dealer exploded
-        //             if (gameDealer->calculateHandTotal() > 21)
-        //             {
-        //                 // Player wins, notify player and all spectators
-        //                 gamePlayer->sendWinner("Player wins!!");
-        //                 for (auto *spectator : Spectatorlist)
-        //                 {
-        //                     spectator->sendWinner("Player wins!!");
-        //                 }
-        //             }
+                    // if dealer exploded
+                    if (gameDealer->calculateHandTotal() > 21)
+                    {
+                        // Player wins, notify player and all spectators
+                        gamePlayer->sendWinner("Player wins!!");
+                        for (auto *spectator : Spectatorlist)
+                        {
+                            spectator->sendWinner("Player wins!!");
+                        }
+                    }
 
-        //             // compare score
-        //             if (gameDealer->calculateHandTotal() > gamePlayer->calculateHandTotal())
-        //             {
-        //                 // dealer wins, notify player and all spectators
-        //                 gamePlayer->sendWinner("Dealer wins!!");
+                    // compare score
+                    if (gameDealer->calculateHandTotal() > gamePlayer->calculateHandTotal())
+                    {
+                        // dealer wins, notify player and all spectators
+                        gamePlayer->sendWinner("Dealer wins!!");
 
-        //                 for (auto *spectator : Spectatorlist)
-        //                 {
-        //                     spectator->sendWinner("Dealer wins!!");
-        //                 }
-        //             }
-        //             else
-        //             {
-        //                 // Player wins, notify player and all spectators
-        //                 gamePlayer->sendWinner("Player wins!!");
-        //                 for (auto *spectator : Spectatorlist)
-        //                 {
-        //                     spectator->sendWinner("Player wins!!");
-        //                 }
-        //             }
+                        for (auto *spectator : Spectatorlist)
+                        {
+                            spectator->sendWinner("Dealer wins!!");
+                        }
+                    }
+                    else
+                    {
+                        // Player wins, notify player and all spectators
+                        gamePlayer->sendWinner("Player wins!!");
+                        for (auto *spectator : Spectatorlist)
+                        {
+                            spectator->sendWinner("Player wins!!");
+                        }
+                    }
 
-        //             gamePlayer->askContinue();
-        //             if (gamePlayer->getContinue())
-        //             {
-        //                 // nothing change
-        //             }
-        //             else
-        //             {
-        //                 // let the connetion close
-        //                 gamePlayer->closeConneton();
-        //                 // then set the first spectator as the player
-        //                 // assign the member to here
-        //                 gamePlayer = new Player(Spectatorlist[0]->getSocket(), Spectatorlist[0]->getRoomId(), this->write, this->read);
-        //                 // reset the first place of vector list
-        //                 // Removing the first element
-        //                 Spectatorlist.erase(Spectatorlist.begin());
-        //             }
+                    gamePlayer->askContinue();
+                    if (gamePlayer->getContinue())
+                    {
+                        // nothing change
+                    }
+                    else
+                    {
+                        // let the connetion close
+                        gamePlayer->closeConneton();
+                        // then set the first spectator as the player
+                        // assign the member to here
+                        gamePlayer = new Player(Spectatorlist[0]->getSocket(), Spectatorlist[0]->getRoomId(), this->write, this->read);
+                        // reset the first place of vector list
+                        // Removing the first element
+                        Spectatorlist.erase(Spectatorlist.begin());
+                    }
 
-        //             if (gamePlayer == nullptr && Spectatorlist.size() == 0)
-        //             {
+                    if (gamePlayer == nullptr && Spectatorlist.size() == 0)
+                    {
 
-        //                 // terminate the thread if there is no player and the list is empty
-        //                 // std::cout << "Thread has finished execution.\n";
-        //                 // return 0;  then terminat
-        //                 continueFlag = false;
-        //             }
-        //             else
-        //             {
-        //                 continueFlag == true;
-        //             }
-        //         }
+                        // terminate the thread if there is no player and the list is empty
+                        // std::cout << "Thread has finished execution.\n";
+                        // return 0;  then terminat
+                        continueFlag = false;
+                    }
+                    else
+                    {
+                        continueFlag == true;
+                    }
+                }
 
-        //         std::this_thread::sleep_for(std::chrono::seconds(10));
-        //         std::cout << "Game room ending...\n";
-        //         return 0;
-        //     }
-        //     catch (std::string &e)
-        //     {
+                std::this_thread::sleep_for(std::chrono::seconds(10));
+                std::cout << "Game room ending...\n";
+                return 0;
+            }
+            catch (std::string &e)
+            {
 
-        //         // system output
-        //         std::cout << "cannot create server" << std::endl;
-        //     }
-        // }
-        // catch (std::string &e)
-        // {
-        //     // system output
-        //     std::cout << e << std::endl;
-        // }
+                // system output
+                std::cout << "cannot create server" << std::endl;
+            }
+        }
+        catch (std::string &e)
+        {
+            // system output
+            std::cout << e << std::endl;
+        }
+        return 0;
     };
 };
 
@@ -543,6 +537,10 @@ int main()
 
     std::cout << "Server started. Listening for connections...\n";
 
+    Semaphore write = Semaphore("write" + count, 1, true);
+    Semaphore read = Semaphore("read" + count, 0, true);
+
+    Shared<MyShared> sharedMemory("sharedMemory", true);
     // Accept connections and create game rooms
     try
     {
@@ -551,14 +549,11 @@ int main()
             Socket newConnection = server.Accept();
             count++;
             std::cout << "Accepted new connection. Starting a game room...\n";
-            Shared<MyShared> sharedMemory("sharedMemory", true);
+
             if (count < 3)
             {
-                Semaphore write = Semaphore("write" + count, 1, true);
-                Semaphore read = Semaphore("read" + count, 0, true);
-                GameRoom *game = new GameRoom(count, new Player(newConnection, count, write, read), write, read);
                 // Start a new game room for each connection
-                gameRooms.push_back(game);
+                gameRooms.push_back(new GameRoom(count, new Player(newConnection, count, write, read), write, read));
             }
             else
             {
